042e0fbba8baccb5658ab4b51a00e6a0a344b37f
CIVILLIAN98 <civillian1998@gmail.com>
2023-04-27 23:24:09 +0900

    foundation

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import { Container } from "./style";
 
 export const Register = () => {
-  return <Container>Register</Container>;
+  return (
+    <Container>
+      <h1>Register</h1>
+    </Container>
+  );
 };
